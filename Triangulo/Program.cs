14e57b7d552cd48b80c60cdc668b84491c2310e3
slavi <slavi@DESKTOP-7HH5DMR>
2023-04-06 14:12:59 -0300

    Add project files.

---
+++ Triangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("Vamos verificar o tipo de seu triangulo.");
+            Console.WriteLine("Informe o primeiro lado do triangulo: ");
+            triangulo.lado1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo lado do triangulo: ");
+            triangulo.lado2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o terceiro lado do triangulo: ");
+            triangulo.lado3 = int.Parse(Console.ReadLine());
+
+            triangulo.tipoTriangulo = triangulo.DetectarTipoTriangulo(triangulo.lado1, triangulo.lado2, triangulo.lado3);
+
+            Console.WriteLine($"O seu triangulo{triangulo.tipoTriangulo}");
+        }
+    }
+}
---
+++ Triangulo/Triangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    internal class Triangulo
+    {
+        public int lado1;
+        public int lado2;
+        public int lado3;
+
+        public string tipoTriangulo;
+        public string DetectarTipoTriangulo(int x, int y, int z) 
+        {
+            if(x > y + z)
+            {
+                tipoTriangulo = ", não é um triangulo";
+            }
+            else
+            {
+                if(x == y && y == z && z == x)
+                {
+                    tipoTriangulo = " é Eqüilátero.";
+                }
+                else  if(x != y && y != z && z != x)
+                {
+                    tipoTriangulo = " é Escaleno.";
+                }
+                else if(x == y && x != z || x == z && x != y || y == x && y != z || y == z && y != x || z == x && z != y || z == y && z != x)
+                {
+                    tipoTriangulo = " é Isósceles.";
+                }
+            }
+
+            return tipoTriangulo;
+        }
+    }
+}
